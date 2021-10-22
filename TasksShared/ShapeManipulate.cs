fd71bffa66860d5e5ed11c0ddb0ef5f803ccf2e9
LRB411-Win10 <yll0620@gmail.com>
2021-10-22 10:11:43 -0500

    add Create_RectNogo, Create_Crossing and Convert XY_Ocenter2OTopLeft

--- TasksShared/ShapeManipulate.cs
+++ TasksShared/ShapeManipulate.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using swf = System.Windows.Forms;
 
 namespace TasksShared
 {
     public class ShapeManipulate
     {
-        public static Ellipse Create_Circle(double Diameter, SolidColorBrush brush_Fill)
+        public static Ellipse Create_Circle(double diameter)
         {/*
             Create the circle
 
@@ -21,13 +18,13 @@ namespace TasksShared
             */
 
             // Create an Ellipse  
-            Ellipse circle = new Ellipse();
+            Ellipse circle = new Ellipse
+            {
 
-            // set the size, position of circleGo
-            circle.Height = Diameter;
-            circle.Width = Diameter;
-
-            circle.Fill = brush_Fill;
+                // set the size, position of circleGo
+                Height = diameter,
+                Width = diameter
+            };
 
             return circle;
         }
@@ -52,5 +49,104 @@ namespace TasksShared
 
             return circle;
         }
+
+
+
+        public static Rectangle Create_NogoRect(double height, double width)
+        {/*
+            Create the circle
+
+            Args:
+                height, width: the height and width of the rectangle in Pixal
+
+            */
+
+            Rectangle rect = new Rectangle
+            {
+                Height = height,
+                Width = width
+            };
+            return rect;
+        }
+
+
+        public static Crossing Create_OneCrossing(double lineLength)
+        {/*
+            Create the Crossing
+
+            Args:
+                lineLength: the length of  in Pixal
+
+            */
+            Crossing crossing = new Crossing(lineLength);
+
+            return crossing;
+        }
+
+
+        public static int[] ConvertXY_OCenter2TopLeft(int[] xy_OCenter, int width, int height)
+        {/*
+            Convert X, Y in Coordinate System with Origin at Center into TopLeft of screen
+
+            Positive is Right and Down
+
+            Args:
+                xy_OCenter: x, y in Coordinate System with Origin at Center of screen
+
+                width, height: the width and height of the screen
+            */
+
+            int[] xy_TopLeft = new int[] { xy_OCenter[0] + width / 2, xy_OCenter[1] + height /2};
+
+            return xy_TopLeft;
+        }
+    }
+
+
+    public class Crossing : Shape
+    {
+        Line horiLine, vertLine;
+        double length;
+        public Crossing(double len)
+        {
+            length = len;
+
+            horiLine = new Line();
+            horiLine.X1 = 0;
+            horiLine.Y1 = 0;
+            horiLine.X2 = len;
+            horiLine.Y2 = horiLine.Y1;
+
+            vertLine = new Line();
+            vertLine.X1 = 0;
+            vertLine.Y1 = 0;
+            vertLine.X2 = vertLine.X1;
+            vertLine.Y2 = len;
+        }
+
+
+        public void Show_Crossing_OTopLeft(int[] cPoint_Pos_OTopLeft)
+        {/*     Show  Crossing Containing One Horizontal Line and One Vertical Line at cPoint_Pos_OTopLeft
+            *   centerPoint_Pos_OCenter: The Center Point X, Y Position of the Two Lines Intersect, Origin at Top Left
+            * 
+             */
+
+            int x0 = cPoint_Pos_OTopLeft[0], y0 = cPoint_Pos_OTopLeft[1];
+            horiLine.X1 = x0 - length / 2;
+            horiLine.X2 = x0 + length / 2;
+            horiLine.Y1 = y0;
+            horiLine.Y2 = y0;
+
+            vertLine.X1 = x0;
+            vertLine.X2 = x0;
+            vertLine.Y1 = y0 - length / 2;
+            vertLine.Y2 = y0 + length / 2;
+
+
+            horiLine.Visibility = Visibility.Visible;
+            vertLine.Visibility = Visibility.Visible;
+        }
+
+        protected override Geometry DefiningGeometry => throw new System.NotImplementedException();
     }
 }
